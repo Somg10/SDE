ab4a5414cb6566289d45bc56c96407547654e549
Som Kr. Gupta <somgupta004@gmail.com>
2022-04-15 22:45:14 +0530

    Create Largest Number formed from an Array.java

---
+++ Largest Number formed from an Array.java
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] arr = br.readLine().trim().split(" ");
+
+            String ans = new Solution().printLargest(arr);
+            System.out.println(ans);
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String printLargest(String[] arr) {
+        // code here
+        Arrays.sort(arr,(s1,s2)->
+        {
+            return (s2+s1).compareTo(s1+s2);
+        });
+        String s="";
+        for(String x:arr){
+            s+=x;
+        }
+        return s;
+    }
+}
